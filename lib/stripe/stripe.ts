9ef1cc15ad3e37c1ed7457c3660cd8ba37b4c03b
johnin007 <johnsmithin007@yahoo.com>
2025-03-26 04:16:52 +0000

    Change API - enhancing logs

--- lib/stripe/stripe.ts
+++ lib/stripe/stripe.ts
@@ -1,6 +1,6 @@
 import Stripe from "stripe";
 
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+export let stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
   // https://github.com/stripe/stripe-node#configuration
   apiVersion: "2022-11-15",
   // Register this as an official Stripe plugin.
