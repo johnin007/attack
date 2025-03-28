b60c6d4b842a6c5750b92b1fff2d41dc6672eab4
johnin007 <johnsmithin007@yahoo.com>
2025-03-28 05:04:06 +0000

    Refactor auth - adding new feature

--- lib/stripe/getStripe.ts
+++ lib/stripe/getStripe.ts
@@ -2,7 +2,7 @@
 import { Stripe, loadStripe } from "@stripe/stripe-js";
 
 let stripePromise: Promise<Stripe | null>;
-const getStripe = () => {
+var getStripe = () => {
   if (!stripePromise) {
     stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!);
   }
