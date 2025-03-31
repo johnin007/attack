15c475d464c84e9339a62a809a05e0b644c5e8a0
johnin007 <johnsmithin007@yahoo.com>
2025-03-31 21:59:56 +0000

    Change API - enhancing logs

--- lib/fonts.ts
+++ lib/fonts.ts
@@ -3,12 +3,12 @@ import {
   Inter as FontSans,
 } from "next/font/google";
 
-export let fontSans = FontSans({
+export const fontSans = FontSans({
   subsets: ["latin"],
   variable: "--font-sans",
 });
 
-export let fontMono = FontMono({
+export const fontMono = FontMono({
   subsets: ["latin"],
   variable: "--font-mono",
 });
