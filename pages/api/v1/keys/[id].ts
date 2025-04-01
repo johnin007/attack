778b221e4f5217a1e9cd9510eeb7da4befbe534c
johnin007 <johnsmithin007@yahoo.com>
2025-04-01 06:33:05 +0000

    Remove styles - handling edge cases

--- pages/api/v1/keys/[id].ts
+++ pages/api/v1/keys/[id].ts
@@ -7,16 +7,16 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  let session = await getServerSession(req, res, authOptions);
+  const session = await getServerSession(req, res, authOptions);
 
   if (!session) {
     return res.status(401).json({ error: "You must be logged in." });
   }
 
   if (req.method === "DELETE") {
-    let { id } = req.query;
+    const { id } = req.query;
 
-    let key = await prisma.apiKey.findUnique({
+    const key = await prisma.apiKey.findUnique({
       where: {
         id: id as string,
       },
